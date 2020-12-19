ac26cdaa4d934e00b5daa972bccf155ef4e4a32e
Chanyeong Kang <rrrfffrrr@naver.com>
2020-12-20 03:21:40 +0900

    Add log & prepare sqlite3

--- sources/index.ts
+++ sources/index.ts
@@ -2,6 +2,10 @@
 import { CreateLogger } from './logger';
 var logger = CreateLogger("logs");
 
+// local db
+logger.info("Initializing local database");
+import sqlite3 from 'sqlite3';
+
 // aws
 logger.info("Initializing AWS SDK");
 import AWS from 'aws-sdk';
@@ -19,30 +23,35 @@ logger.info("Initializing Discord.js");
 import { Message, Client } from 'discord.js';
 import { CommandRouter } from './router';
 let client = new Client();
-
-
-const commandRouter: CommandRouter<Message> = new CommandRouter<Message>();
-commandRouter.add(/^내전서버구동/, async (msg:Message) => {
-    logger.info(`${msg.member?.displayName}(${msg.member?.id}) has authorized.`);
-    //await ec2.startInstances({ DryRun: false, InstanceIds: [targetInstance] }).promise();
-});
-commandRouter.add(/^내전서버정지/, async (msg:Message) => {
-    logger.info(`${msg.member?.displayName}(${msg.member?.id}) has authorized.`);
-    //await ec2.stopInstances({ DryRun: false, InstanceIds: [targetInstance] }).promise();
+client.on('message', (msg) => {
+    logger.debug(msg);
+    commandRouter.route(msg.content, msg);
 });
 
+// discord message callback
+logger.info("Initializing command router");
 function IsAuthorized(msg: Message) {
     return msg.member?.id === process.env.SERVER_OWNER_DISCORD_ID;
 }
-client.on('message', (msg) => {
+const commandRouter: CommandRouter<Message> = new CommandRouter<Message>();
+logger.verbose("Add 내전서버구동 command");
+commandRouter.add(/^내전서버구동/, async (msg:Message) => {
     if (!IsAuthorized(msg)) return;
-    commandRouter.route(msg.content, msg);
+    logger.info(`${msg.member?.displayName}(${msg.member?.id}) has authorized.`);
+    //await ec2.startInstances({ DryRun: false, InstanceIds: [targetInstance] }).promise();
+});
+logger.verbose("Add 내전서버정지 command");
+commandRouter.add(/^내전서버정지/, async (msg:Message) => {
+    if (!IsAuthorized(msg)) return;
+    logger.info(`${msg.member?.displayName}(${msg.member?.id}) has authorized.`);
+    //await ec2.stopInstances({ DryRun: false, InstanceIds: [targetInstance] }).promise();
 });
 
 // Test service and open server
 logger.info("Test services");
 ec2.describeInstances({ DryRun: true, InstanceIds: [targetInstance] }).promise().catch((e: AWSError) => {
     if (e.code === 'DryRunOperation') {
+        logger.verbose("Dry run EC2.describeInstances success.");
         return ec2.startInstances({ DryRun: true, InstanceIds: [targetInstance] }).promise();
     }
 
@@ -51,6 +60,7 @@ ec2.describeInstances({ DryRun: true, InstanceIds: [targetInstance] }).promise()
     process.exitCode = 1;
 }).catch((e: AWSError) => {
     if (e.code === 'DryRunOperation') {
+        logger.verbose("Dry run EC2.startInstances success.");
         return ec2.stopInstances({ DryRun: true, InstanceIds: [targetInstance] }).promise();
     }
 
@@ -59,6 +69,7 @@ ec2.describeInstances({ DryRun: true, InstanceIds: [targetInstance] }).promise()
     process.exitCode = 1;
 }).catch((e: AWSError) => {
     if (e.code === 'DryRunOperation') {
+        logger.verbose("Dry run EC2.stopInstances success.");
         return client.login(process.env.DISCORD_TOKEN);
     }
 
